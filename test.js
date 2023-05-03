e6540fb85d2183f1652eb1c6b1eecf616fc7128c
ErzenXs <njnana2017@gmail.com>
2023-05-03 03:25:48 +0200

    bug fixes

--- test.js
+++ test.js
@@ -1,104 +1,35 @@
-function readTextFromFiles(file1, file2, callback) {
-    const reader1 = new FileReader();
-    const reader2 = new FileReader();
+'use strict';
 
-    // Read file1
-    reader1.onload = function (event) {
-        const text1 = event.target.result;
+const {
+    readInput, writeOutput, getMemoryUsageMessage, elapsedTime, parseLine
+} = require('./utils');
 
-        // Read file2
-        reader2.onload = function (event) {
-            const text2 = event.target.result;
-            callback(text1, text2);
-        };
-        reader2.onerror = function (event) {
-            console.error(`Error reading file2: ${event.target.error}`);
-            callback(text1, null);
-        };
-        reader2.readAsText(file2);
-    };
-    reader1.onerror = function (event) {
-        console.error(`Error reading file1: ${event.target.error}`);
-        callback(null, null);
-    };
-    reader1.readAsText(file1);
+const [filePath, outputToFile = 0, debug = 0] = process.argv.slice(2);
+
+if (!filePath || isNaN(outputToFile) || isNaN(debug)) {
+    console.info(`Usage:
+  node index.js <input-file-path> [output-to-file] [debug]
+
+Options:
+  input-file-path  The path of the input data set.
+  output-to-file   Write the insights to stdout (0) or file (1) [default: 0].
+  debug            Write memory usage and elapsed time information to stdout [default: 0].
+
+Examples:
+  node index.js data/a.txt
+    This will parse the given input data set (data/a.txt)
+    with its submission (data/a.txt.out.txt) then evaluate and
+    print the insights to the stdout.
+
+  node index.js data/d.txt 1
+    This will parse the given input data set (data/d.txt)
+    with its submission (data/d.txt.out.txt) then evaluate and
+    create a file (data/d.txt.insights.txt) containing insights.
+`);
+    return;
 }
 
-const file1Input = document.getElementById('file1'); // Assuming file input elements with IDs 'file1' and 'file2'
-const file2Input = document.getElementById('file2');
-
-let _text1, _text2;
-
-
-file1Input.addEventListener('change', function (event) {
-    const file1 = event.target.files[0];
-    file2Input.addEventListener('change', function (event) {
-        const file2 = event.target.files[0];
-        readTextFromFiles(file1, file2, function (text1, text2) {
-            if (text1 !== null && text2 !== null) {
-                _text1 = text1;
-                _text2 = text2;
-                document.getElementById("btn").classList.remove("hidden");
-            }
-        });
-    });
-});
-
-
-
-
-
-
-
-
-
-function validateData() {
-    let input = _text1;
-    let output = _text2;
-
-    parseInputDataSet(input);
-    parseSubmittedDataSet(output);
-    simulate();
-    let score = createInsights();
-    let r = new Set(results);
-
-    results = [...r];
-
-    let table = generateTable(results);
-
-    document.getElementById("result").innerHTML = score;
-    document.getElementById("result").classList.remove("hidden");
-    document.getElementById("info").classList.remove("hidden");
-    document.getElementById("info").innerHTML = table;
-}
-
-function generateTable(dataArray) {
-    let tableHTML = '<table>';
-    tableHTML += '<thead><tr><th>Constraint</th><th>Result</th></tr></thead>';
-    tableHTML += '<tbody>';
-
-    // Loop through each string in the dataArray and generate a table row
-    for (let i = 0; i < dataArray.length; i++) {
-        let result = '✅'; // default to a checkmark for normal results
-        if (dataArray[i].indexOf('error') !== -1) {
-            result = '❌'; // if the string contains "error", use an x mark instead
-        }
-
-        // Generate the table row with the constraint and result columns
-        tableHTML += '<tr>';
-        tableHTML += `<td>${dataArray[i]}</td>`;
-        tableHTML += `<td>${result}</td>`;
-        tableHTML += '</tr>';
-    }
-
-    tableHTML += '</tbody>';
-    tableHTML += '</table>';
-
-    return tableHTML;
-}
-
-
-
+const elapsed = elapsedTime(+debug);
 const dataset = {
     simulation: {
         duration: 0,
@@ -152,21 +83,6 @@ function parseInputDataSet(data) {
     }
 }
 
-function parseLine(rawLine, stringToNumberArrayIndexes = []) {
-    const parsedLine = rawLine.split(' ');
-
-    return stringToNumberArrayIndexes.length
-        ? parsedLine.map((value, i) =>
-            stringToNumberArrayIndexes.includes(i) ? +value : value
-        )
-        : parsedLine;
-}
-
-let results = [
-
-]
-
-
 function parseSubmittedDataSet(data) {
     const lines = data.split('\n');
     const numIntersections = +lines.shift();
@@ -179,31 +95,22 @@ function parseSubmittedDataSet(data) {
         const numIncomingStreets = lines[currentLine + i + 1];
 
         if (typeof numIncomingStreets === 'undefined') {
-            results.push("Submission file has fewer lines than expected Error-Code:" + `Unexpected EOF (end of file) at line ${currentLine + i + 2}`);
             throw new Error([
                 'Submission file has fewer lines than expected',
                 `Unexpected EOF (end of file) at line ${currentLine + i + 2}`,
             ].join('. '));
-        } else {
-            results.push("Submission file has normal number of lines");
         }
 
         if (isNaN(numIncomingStreets)) {
-            results.push(`Invalid number of elements found at line ${currentLine + i + 3}`);
             throw new Error(
                 `Invalid number of elements found at line ${currentLine + i + 3}`
             );
-        } else {
-            results.push("Submission file has normal number of elements");
         }
 
         if (intersectionSchedulesById[intersectionId]) {
-            results.push(`More than one adjustment was provided for intersection ${intersectionId}.`);
             throw new Error(
                 `More than one adjustment was provided for intersection ${intersectionId}.`
             );
-        } else {
-            results.push("Submission file has normal number of adjustments");
         }
 
         intersectionSchedulesById[intersectionId] = true;
@@ -219,35 +126,25 @@ function parseSubmittedDataSet(data) {
                 dataset.streets[streetName] &&
                 dataset.streets[streetName].end !== +intersectionId
             ) {
-                results.push("Submission file has invalid street names " + `Error-Code: The schedule of intersection ${intersectionId} refers to street ${streetName}, but that street does not enter this intersection, so it cannot be part of the intersection schedule.`);
                 throw new Error([
                     `The schedule of intersection ${intersectionId} refers to street`,
                     `${streetName}, but that street does not enter this intersection,`,
                     'so it cannot be part of the intersection schedule.',
                 ].join(' '));
-            } else {
-                results.push("Submission file has normal street names");
             }
 
             if (isNaN(schedule)) {
-                results.push("Submission file has invalid schedule " + `Error-Code: The schedule of street ${streetName} has a duration for green light that is not a number: ${schedule}.`);
                 throw new Error([
                     `The schedule of street ${streetName} has a duration for green light`,
                     `that is not a number: ${schedule}.`,
                 ].join(' '));
-            } else {
-                results.push("Submission file has normal schedule for green light");
             }
 
-
             if (schedule < 1 || schedule > dataset.simulation.duration) {
-                results.push("Submission file has invalid schedule " + `Error-Code: The schedule of street ${streetName} should have duration for green light that is between 1 and ${dataset.simulation.duration}.`);
                 throw new Error([
                     `The schedule of street ${streetName} should have duration`,
                     `for green light that is between 1 and ${dataset.simulation.duration}.`,
                 ].join(' '));
-            } else {
-                results.push("Submission file has normal schedule for duration");
             }
 
             dataset.streets[streetName].schedule = {
@@ -269,7 +166,6 @@ function parseSubmittedDataSet(data) {
     averageIntersectionSchedules.greenCycles = averageIntersectionSchedules.totalCycles / numSchedules;
     averageIntersectionSchedules.totalCycles /= dataset.simulation.numIntersections;
 }
-
 
 function simulate() {
     let remainingTime = dataset.simulation.duration + 1;
@@ -336,16 +232,16 @@ function simulate() {
     }
 }
 
-let outputToFile = 0;
-
 function createInsights() {
-    const toPercentage = value => `${(value * 100).toFixed(0)}%`;
+    const noColor = +outputToFile === 1;
+    const yellow = value => noColor ? value : `\u001B[33m${value}\u001B[0m`;
+    const formatNumber = value => yellow(value.toLocaleString('en-US'));
+    const toPercentage = value => yellow(`${(value * 100).toFixed(0)}%`);
 
     const { numIntersections, numCars, bonusPoint } = dataset.simulation;
     const arrivedCars = dataset.cars
         .filter(car => car.arrived)
         .sort((a, b) => a.commuteTime - b.commuteTime);
-
     const numArrivedCars = arrivedCars.length;
     const score = arrivedCars.reduce((sum, car) => sum + car.score, 0);
     const earlyArrivalBonus = arrivedCars.reduce((sum, car) =>
@@ -356,28 +252,53 @@ function createInsights() {
     ) / numArrivedCars).toFixed(2);
 
     const arrivedCarsInsights = [
-        `${numArrivedCars} of ${numCars}`,
+        `${formatNumber(numArrivedCars)} of ${formatNumber(numCars)}`,
         `cars arrived before the deadline (${toPercentage(numArrivedCars / numCars)}).`,
     ];
 
     if (arrivedCars.length) {
-        // arrivedCarsInsights.push(...[
-        //     `The earliest car arrived at its destination after ${arrivedCars[0].commuteTime}`,
-        //     `seconds scoring ${arrivedCars[0].score} points, whereas the last`,
-        //     `car arrived at its destination after ${arrivedCars[numArrivedCars - 1].commuteTime}`,
-        //     `seconds scoring ${arrivedCars[numArrivedCars - 1].score} points.`,
-        //     `Cars that arrived within the deadline drove for an average of ${averageCommuteTime}`,
-        //     'seconds to arrive at their destination.',
-        // ]);
+        arrivedCarsInsights.push(...[
+            `The earliest car arrived at its destination after ${formatNumber(arrivedCars[0].commuteTime)}`,
+            `seconds scoring ${formatNumber(arrivedCars[0].score)} points, whereas the last`,
+            `car arrived at its destination after ${formatNumber(arrivedCars[numArrivedCars - 1].commuteTime)}`,
+            `seconds scoring ${formatNumber(arrivedCars[numArrivedCars - 1].score)} points.`,
+            `Cars that arrived within the deadline drove for an average of ${formatNumber(averageCommuteTime)}`,
+            'seconds to arrive at their destination.',
+        ]);
     }
 
     return [
         [
-            `The submission file scored ${score} points.`,
+            `The submission scored ${formatNumber(score)} points. This is the sum of`,
+            `${formatNumber(bonusPoint * numArrivedCars)} bonus points`,
+            `for cars arriving before the deadline (${formatNumber(bonusPoint)}`,
+            `points each) and ${formatNumber(earlyArrivalBonus)} points for early arrival times.`,
         ].join(' '),
         arrivedCarsInsights.join(' '),
         [
+            `The schedules for the ${formatNumber(numIntersections)}`,
+            'traffic lights had an average total cycle length of',
+            `${yellow(averageIntersectionSchedules.totalCycles.toFixed(2))} seconds.`,
+            'A traffic light that turned green was scheduled to stay green for',
+            `${yellow(averageIntersectionSchedules.greenCycles.toFixed(2))} seconds on average.`,
             '\n',
         ].join(' '),
     ].join('\n\n');
 }
+
+function evaluate([inputDataSet, submittedDataSet]) {
+    parseInputDataSet(inputDataSet);
+    parseSubmittedDataSet(submittedDataSet);
+    simulate();
+
+    return createInsights();
+}
+
+Promise.all([readInput(filePath), readInput(`${filePath}.out.txt`)])
+    .then(evaluate)
+    .then(output => !+outputToFile
+        ? (console.info(output), Promise.resolve())
+        : writeOutput(`${filePath}.insights.txt`, output)
+    )
+    .then(() => elapsed.log(`${getMemoryUsageMessage()}\n\nElapsed time`).reset())
+    .catch(console.error);
